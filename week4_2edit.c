e7467f95d9a62528c2b6fa37811f9855a79794fd
johnybravou <111229430+johnybravou@users.noreply.github.com>
2022-09-10 14:06:38 +0700

    Add files via upload

---
+++ week4_2edit.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if(a+b>=b+c && a+b>=a+c )
+    {
+        printf("%d",a+b);
+    }
+    else if(b+c>=a+c)
+    {
+        printf("%d",b+c);
+    }
+    else
+    {
+        printf("%d",c+a);
+    }
+}
